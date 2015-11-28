040e212d8c6f76ffc75263af463b9968b6aca4de
Jeff Langr <jeff.langr@outpace.com>
2015-11-28 11:32:53 -0700

    init

---
+++ buildAll.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+javac -classpath .:hamcrest-core-1.3.jar:junit-4.12.jar *.java
+
---
+++ runAllTests.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -classpath .:hamcrest-core-1.3.jar:junit-4.12.jar org.junit.runner.JUnitCore SoundexTest
